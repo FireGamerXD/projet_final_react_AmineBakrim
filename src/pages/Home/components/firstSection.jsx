04c0311d2a0bba5cfa16fcfd5ef71df51fc7dc69
FireGamerXD <amineg715@gmail.com>
2024-02-15 12:24:15 +0100

    blog

--- src/pages/Home/components/firstSection.jsx
+++ src/pages/Home/components/firstSection.jsx
@@ -12,6 +12,25 @@ import { hover } from '@testing-library/user-event/dist/hover';
 export const FirstSection = () => {
 
 
+
+  const [productname, setProductname] = useState([
+    {
+      productname: "product 1"
+    },
+    {
+      productname: "product 2"
+    },
+    {
+      productname: "product 3"
+    },
+    {
+      productname: "product 4"
+    },
+    {
+      productname: "product 5"
+    },
+  ])
+
     const [products, setProducts] = useState([
 
   {
@@ -24,7 +43,7 @@ export const FirstSection = () => {
 
   {
     productname: "Whey Protein 2",
-    productprice: "50$",
+    productprice: "40$",
     productdesc: "this is the best whey protein that u can buy in the world but it now",
     productimg: wheyprotein,
   },
@@ -32,7 +51,7 @@ export const FirstSection = () => {
 
   {
     productname: "Whey Protein 3",
-    productprice: "50$",
+    productprice: "60$",
     productdesc: "this is the best whey protein that u can buy in the world but it now",
     productimg: wheyprotein,
   },
@@ -40,7 +59,7 @@ export const FirstSection = () => {
 
   {
     productname: "Whey Protein 4",
-    productprice: "50$",
+    productprice: "80$",
     productdesc: "this is the best whey protein that u can buy in the world but it now",
     productimg: wheyprotein,
   },
@@ -48,16 +67,17 @@ export const FirstSection = () => {
 
   {
     productname: "Whey Protein 5",
-    productprice: "50$",
+    productprice: "20$",
     productdesc: "this is the best whey protein that u can buy in the world but it now",
     productimg: wheyprotein,
   }
 
 ])
 
-
-
-
+// const test = () =>{
+//   const randomElement = products[Math.floor(Math.random() * products.length)];
+//   const testtt = document.getElementById('tes').innerHTML = randomElement
+// }
 
     return (
         <>
@@ -180,7 +200,7 @@ class="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 
 
 {/* cards part 3 */}
 
-<div className="cards5_6 xl:block flex flex-col items-center">
+<div className="xl:mt-0 mt-8 cards5_6 xl:block flex flex-col items-center">
 
 <div className="card1 bg-gray-200 xl:w-[25vw] h-[60vh] w-[75%] flex flex-col justify-center items-center rounded-lg hover:bg-slate-500 transition">
 
@@ -283,41 +303,33 @@ class="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 
 
 
 {/* random Sec */}
-
-
-
-
-
-{
-  products.map((element, index) =>
-  <>
-  <div className="mt-44 bg-gray-100 w-[100%] h-[90vh] flex items-center justify-evenly">
-  <div className=" bg-red-400 w-[35%] h-[75%] flex justify-around">
+  <div className="mt-44 mb-44 bg-gray-100 xl:w-[100%] xl:h-[90vh]  h-[140vh] flex xl:flex-row flex-col items-center xl:justify-evenly xl:gap-0 gap-8">
+  <div className=" bg-red-400 xl:mt-0 mt-8  xl:w-[35%] xl:h-[75%] w-[90vw]  flex justify-around">
           <div className="textall h-[100%] pt-64">
       <h1 className='text-5xl text-white font-bold flex justify-center items-center p-4'>Be aesthetic</h1>
       <p className='text-2xl font-thin text-white text-center cursor-pointer hover:underline transition'>View Our Collection</p></div>
   </div>
 
-    <div className="whitebg  bg-white w-[35%] h-[75%] flex flex-col justify-center items-center">
+    <div className="whitebg  bg-white xl:w-[35%] xl:h-[75%] w-[90vw] h-[70%] xl:mb-0 mb-8 flex flex-col justify-center items-center">
         <div className=" w-[55%] h-[60%] mb-2 hover:scale-105 transition">
-        <img src={element.productimg} alt="" className='' />
+        <img src={wheyprotein} alt="" className='' />
         </div>
-        <h1 className='text-2xl'>{element.productname}</h1>
-        <p className='text-xl font-extralight'>{element.productprice}</p>
+        <h1 className='text-2xl'>Whey Protein</h1>
+        <p className='text-xl font-extralight'>50£</p>
         <div className="boxs flex flex-row gap-4 mt-4">
-            <div className="bg-white border w-[5vw] h-[4vw] flex flex-col justify-center items-center">
+            <div className="bg-white border w-[15vw] xl:w-[5vw] xl:h-[4vw] flex flex-col justify-center items-center">
               <p className='text-lg font-serif'>112</p>
               <p className='font-thin'>days</p>
             </div>
-            <div className="bg-white border w-[5vw] h-[4vw] flex flex-col justify-center items-center">
+            <div className="bg-white border w-[15vw] xl:w-[5vw] xl:h-[4vw] flex flex-col justify-center items-center">
               <p className='text-lg font-serif'>10</p>
               <p className='font-thin'>Hrs</p>
             </div>
-            <div className="bg-white border w-[5vw] h-[4vw] flex flex-col justify-center items-center">
+            <div className="bg-white border w-[15vw] xl:w-[5vw] xl:h-[4vw] flex flex-col justify-center items-center">
               <p className='text-lg font-serif'>55</p>
               <p className='font-thin'>mins</p>
             </div>
-            <div className="bg-white border w-[5vw] h-[4vw] flex flex-col justify-center items-center">
+            <div className="bg-white border w-[15vw] xl:w-[5vw] xl:h-[4vw] flex flex-col justify-center items-center">
               <p className='text-lg font-serif'>48</p>
               <p className='font-thin'>secs</p>
             </div>
@@ -326,11 +338,63 @@ class="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 
 
 
 </div>
-  </>
-  )
-}
 
 
+
+{/* Blog Sec */}
+
+<div className="allcontainerblog xl:h-[120vh] w-[100%]">
+<p className='font-semibold text-4xl text-center xl:mb-16'>OUR <span className='text-white bg-secondary-color rounded-lg p-2 xl:hover:bg-red-400 transition'>BLOG</span> </p>
+
+<div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5 xl:w-[75%] ml-0 xl:ml-44">
+    <div class="rounded overflow-hidden shadow-lg h-[100%] xl:hover:scale-105 xl:transition">
+      <img class="w-[50%] ml-24" src={wheyprotein} alt="Mountain" />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Whey Protein</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-red-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Gym</span>
+        <span class="inline-block bg-red-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Strong</span>
+        <span class="inline-block bg-red-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Protein</span>
+      </div>
+    </div>
+    <div class="rounded overflow-hidden shadow-lg h-[100%] xl:hover:scale-105 xl:transition">
+      <img class="w-[50%] ml-24 flex justify-center" src={wheyprotein} alt="Mountain" />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Creatine</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-red-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Creatine</span>
+        <span class="inline-block bg-red-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#strenght</span>
+        <span class="inline-block bg-red-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#power</span>
+      </div>
+    </div>
+
+    <div class="rounded overflow-hidden shadow-lg h-[100%] xl:hover:scale-105 xl:transition">
+      <img class="w-[50%] ml-24 flex justify-center" src={wheyprotein} alt="Mountain" />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">PreWorkOut</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-red-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#PreWorkOut</span>
+        <span class="inline-block bg-red-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Energy</span>
+        <span class="inline-block bg-red-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#time</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+{/* Follow Us Sec */}
 
 
 
